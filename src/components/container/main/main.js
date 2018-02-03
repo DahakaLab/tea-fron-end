01f8c2c716633acd270b89933a1dba45343c2b96
DahakaLab <Dahaka_Lab@mail.ru>
2018-02-03 23:44:35 +0300

    Add inscription Test in main.js

--- src/components/container/main/main.js
+++ src/components/container/main/main.js
@@ -4,6 +4,7 @@ export default class Main extends Component{
     render(){
         return(
             <div className="main ">
+                <h1>!TEST!</h1>
                 <h1 className="red ">Ремонт ноутбуков</h1>
                 <h2 className="green ">Чайный сервис</h2>
                 <article>
